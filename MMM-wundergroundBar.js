4a4dcc648d6340d8489ecc80e7bef8affe475608
Fifteen 15 Studios <havens1515@gmail.com>
2022-07-02 18:54:09 -0400

    Fix some things
    Only refresh when notification received from this module
    Remove initialLoadDelay, which did nothing
    Remove header code - no reason in overriding default MM functionality
    Remove some unused code
    Fix some output formatting

--- MMM-wundergroundBar.js
+++ MMM-wundergroundBar.js
@@ -13,13 +13,10 @@ Module.register("MMM-wundergroundBar", {
         tempUnits: "e", // e or m
         latitude: "40.6892534",
         longitude: "-74.0466891",
-        useHeader: false,
-        header: "Your Header", // Any text you want. useHeader must be true
         maxWidth: "100%",
         animationSpeed: 3000,
         iconSize: "2%",
         showText: true,
-        initialLoadDelay: 4250,
         retryDelay: 2500,
         showCurrentText: true,
         language: "en-US",
@@ -36,6 +33,7 @@ Module.register("MMM-wundergroundBar", {
 		this.url = 'https://api.weather.com/v3/wx/forecast/daily/5day?geocode=' + this.config.latitude + "," + this.config.longitude + '&units=' + this.config.tempUnits + "&language=" + this.config.language + "&format=json&apiKey=" + this.config.apiKey
 		console.log(this.name + ": url: " + this.url);
         this.forecast = [];
+        this.getWeather();
         this.scheduleUpdate();
     },
 
@@ -62,26 +60,31 @@ Module.register("MMM-wundergroundBar", {
         var text = "";
         if(days[index] != "") {
             text = days[index] + ": " + forecast.calendarDayTemperatureMax[index / 2] + " ";
-        }
-        if(this.config.showText && titles[index] != "") {
-            text = text + titles[index] + " &nbsp";
+			
+			if(this.config.showText && titles[index] != "") {
+				text = text + titles[index] + " &nbsp";
+			}
+			
+			if(iconCodes[index] != "unknown") {
+				text = text + " &nbsp<img src=https://www.wunderground.com/static/i/c/v4/" + iconCodes[index] + ".svg width=" + this.config.iconSize + " height=" + this.config.iconSize + ">";
+			}
+			
+			text = text + " &nbsp &nbsp  &nbsp &nbsp "
         }
         
-		if(iconCodes[index] != "unknown") {
-			text = text + " &nbsp<img src=https://www.wunderground.com/static/i/c/v4/" + iconCodes[index] + ".svg width=" + this.config.iconSize + " height=" + this.config.iconSize + ">";
-		}
         if(days[index+1] != "") {
-		    text = text + " &nbsp &nbsp  &nbsp &nbsp " + days[index+1] + ": " + forecast.calendarDayTemperatureMin[index / 2] + " ";
+		    text = text + days[index+1] + ": " + forecast.calendarDayTemperatureMin[index / 2] + " ";
+			
+			if(this.config.showText && titles[index+1] != "") {
+				text = text + titles[index+1] + " &nbsp";
+			}
+			if(iconCodes[index+1] != "unknown") {
+				text = text +  " &nbsp<img src=https://www.wunderground.com/static/i/c/v4/" + iconCodes[index+1] + ".svg width=" + this.config.iconSize + " height=" + this.config.iconSize + ">";
+			}
         }
-        if(this.config.showText && titles[index+1] != "") {
-            text = text + titles[index+1] + " &nbsp";
-        }
-		if(iconCodes[index] != "unknown") {
-			text = text +  " &nbsp<img src=https://www.wunderground.com/static/i/c/v4/" + iconCodes[index+1] + ".svg width=" + this.config.iconSize + " height=" + this.config.iconSize + ">";
-		}
 
         return text;
-},
+	},
 
     getDom: function() {
 
@@ -93,13 +96,6 @@ Module.register("MMM-wundergroundBar", {
             wrapper.innerHTML = "Boring weather . . .";
             wrapper.classList.add("bright", "light", "small");
             return wrapper;
-        }
-
-        if (this.config.useHeader != false) {
-            var header = document.createElement("header");
-            header.classList.add("xsmall", "bright", "light");
-            header.innerHTML = this.config.header;
-            wrapper.appendChild(header);
         }
 
         var forecast = this.forecast;
@@ -146,8 +142,8 @@ Module.register("MMM-wundergroundBar", {
 						"<img src=https://www.wunderground.com/static/i/c/v4/clear.svg width=" + this.config.iconSize + " height=" + this.config.iconSize + "> &nbspSunrise: &nbsp" + forecast.sunriseTimeLocal.toString().split("T")[1].split("-")[0] + " &nbsp &nbsp  &nbsp &nbsp " +
 						"<img src=https://www.wunderground.com/static/i/c/v4/nt_clear.svg width=" + this.config.iconSize + " height=" + this.config.iconSize + "> &nbspSunset: &nbsp" + forecast.sunsetTimeLocal.toString().split("T")[1].split("-")[0] + " &nbsp &nbsp  &nbsp &nbsp " +
                         this.getDayHTML(forecast, 0) + " <BR> " + 
-                        this.getDayHTML(forecast, 2) + 
-                        this.getDayHTML(forecast, 4) + 
+                        this.getDayHTML(forecast, 2) + " &nbsp &nbsp  &nbsp &nbsp " + 
+                        this.getDayHTML(forecast, 4) + " &nbsp &nbsp  &nbsp &nbsp " +
                         this.getDayHTML(forecast, 6);
         wrapper.appendChild(daily);
 	
@@ -165,7 +161,6 @@ Module.register("MMM-wundergroundBar", {
         setInterval(() => {
             this.getWeather();
         }, this.config.updateInterval);
-        this.getWeather(this.config.initialLoadDelay);
     },
 
     getWeather: function() {
@@ -173,13 +168,11 @@ Module.register("MMM-wundergroundBar", {
     },
 
     socketNotificationReceived: function(notification, payload) {
-		var time = this.config.initialLoadDelay;
         if (notification === "WEATHER_RESULT") {
             this.processWeather(payload);
 
-            time = time + this.config.animationSpeed;
+            time = this.config.animationSpeed;
+			this.updateDom(time);
         }
-        this.updateDom(time);
     },
 });
-
